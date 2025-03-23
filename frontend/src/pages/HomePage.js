e8d14f15af0ac7ca60b89d7ceeb54326f9889501
ArnavGupta-codes <guptaarnav631@gmail.com>
2025-03-23 20:21:16 +0530

    Final!!!

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,15 +1,12 @@
 import React from 'react'
-
 import Search from "../components/Search";
 import Upload from "../components/Upload";
-
 
 function HomePage() {
     return (
         <div className="outer">
             <Upload />
             <Search />
-
         </div>
     );
 }
